c9e4f641627f9fada4eb0113b024e198df376106
Brock Allen <brockallen@gmail.com>
2013-05-28 12:32:13 -0400

    Add pure config mechanism for SAM/FAM static helpers

---
+++ IdentityModel/Thinktecture.IdentityModel/Web/Configuration/SessionConfigurationModule.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Thinktecture.IdentityModel.Web;
+
+namespace Thinktecture.IdentityModel.Web.Configuration
+{
+    public class SessionConfigurationModule : IHttpModule
+    {
+        static public bool CacheSessionsOnServer { get; set; }
+        static public bool EnableSlidingSessionExpirations { get; set; }
+        static public bool OverrideWSFedTokenLifetime { get; set; }
+        static public bool SuppressLoginRedirectsForApiCalls { get; set; }
+        static public bool SuppressSecurityTokenExceptions { get; set; }
+        static public bool EnablePersistentSessions { get; set; }
+
+        public void Init(HttpApplication application)
+        {
+            if (CacheSessionsOnServer)
+            {
+                PassiveModuleConfiguration.CacheSessionsOnServer();
+            }
+
+            if (EnableSlidingSessionExpirations)
+            {
+                PassiveModuleConfiguration.EnableSlidingSessionExpirations();
+            }
+
+            if (OverrideWSFedTokenLifetime)
+            {
+                PassiveModuleConfiguration.OverrideWSFedTokenLifetime();
+            }
+
+            if (SuppressLoginRedirectsForApiCalls)
+            {
+                PassiveModuleConfiguration.SuppressLoginRedirectsForApiCalls();
+            }
+
+            if (SuppressSecurityTokenExceptions)
+            {
+                PassiveModuleConfiguration.SuppressSecurityTokenExceptions();
+            }
+
+            if (EnablePersistentSessions)
+            {
+                PassiveModuleConfiguration.EnablePersistentSessions();
+            }
+        }
+        
+        public void Dispose()
+        {
+        }
+    }
+}
